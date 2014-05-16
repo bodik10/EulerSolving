92f6edbfead5c558a52ae9179ee0cda761e6c408
Bohdan <bohdan.fedys@gmail.com>
2014-05-16 16:49:14 +0300

    Finding a solution by Brute-force

---
+++ euler 068/euler 068.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.link = None
+    def linkto(self, node):
+        self.link = node
+    def sum(self):
+        return self.value + self.link.value + self.link.link.value 
+    def __repr__(self):
+        return "(%d) -> (%d) -> (%d)" % (self.value, self.link.value, self.link.link.value)
+    
+N = 3
+
+allcomb = permutations(range(1, N*2+1))
+
+
+for comb in allcomb:
+
+    N_gon = [Node(i) for i in comb[N:]]
+    outer = [Node(i) for i in comb[:N]]
+    for i in range(1, N+1):
+        N_gon[i-1].linkto(N_gon[i%N])
+        outer[i-1].linkto(N_gon[i-1])
+    sums = [node.sum() for node in outer]
+    if len(set(sums)) == 1:
+        print (sums[0], outer)
+
